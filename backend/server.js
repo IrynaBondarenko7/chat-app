1faee89a994277b1618715fc31ac3c9f8cbb1a04
Iryna Bondarenko <ira.bondarenko@ukr.net>
2024-12-17 16:29:32 +0000

    completed signup auth controller & created generateToken utils function

--- backend/server.js
+++ backend/server.js
@@ -4,15 +4,17 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  res.send("test");
-});
+dotenv.config();
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   res.send("test");
+// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
