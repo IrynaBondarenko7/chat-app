d5920056e87bd905ba2bd33083be971fa80b20c8
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-03 19:42:56 +0000

    added getMessages controller

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -23,11 +23,33 @@ export const sendMessage = async (req, res) => {
       conversation.messages.push(newMessage._id);
     }
 
+    // SOCKET IO FUNCTIONALITY
+
     await Promise.all([conversation.save(), newMessage.save()]);
 
     res.status(201).json(newMessage);
   } catch (error) {
     console.log("Error in sendMessage controller", error.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+export const getMessages = async (req, res) => {
+  try {
+    const { id: userToChatId } = req.params;
+    const senderId = req.user._id;
+
+    const conversation = await Conversation.findOne({
+      participants: { $all: [senderId, userToChatId] },
+    }).populate("messages"); //not reference but actual messages
+
+    if (!conversation) return res.status(200).json([]);
+
+    const messages = conversation.messages;
+
+    res.status(200).json(messages);
+  } catch (error) {
+    console.log("Error in getMessages controller", error.message);
     res.status(500).json({ error: "Internal Server Error" });
   }
 };
