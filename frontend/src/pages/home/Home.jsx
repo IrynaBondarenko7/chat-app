9df909ae1eca5db7de2225d06b01a3c1d34ec5ce
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-10 20:28:24 +0000

    created messages container component

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
+import MessageContainer from "../../components/sidebar/messages/MessageContainer";
 import Sidebar from "../../components/sidebar/Sidebar";
 
 const Home = () => {
   return (
     <div className="flex h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400/0 bg-clip-padding backdrop-blur-lg">
       <Sidebar />
+      <MessageContainer />
     </div>
   );
 };
