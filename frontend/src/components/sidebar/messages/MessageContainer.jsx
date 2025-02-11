0b1963e33b93167c44e466890d33a671e2d7ee21
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-11 20:42:10 +0000

    added noChatSelected component

--- frontend/src/components/sidebar/messages/MessageContainer.jsx
+++ frontend/src/components/sidebar/messages/MessageContainer.jsx
@@ -1,15 +1,24 @@
 import MessageInput from "./MessageInput";
 import Messages from "./Messages";
 
+import NoChatSelected from "./NoChatSelected";
+
 const MessageContainer = () => {
+  const noChatSelected = true;
   return (
     <div className="md:min-w-[450px] flex flex-col">
-      <div className="bg-slate-500 px-4 py-2 mb-2">
-        <span className="label-text ">To:</span>
-        <span className="text-gray-400 font-bold">John Doe</span>
-      </div>
-      <Messages />
-      <MessageInput />
+      {noChatSelected ? (
+        <NoChatSelected />
+      ) : (
+        <>
+          <div className="bg-slate-500 px-4 py-2 mb-2">
+            <span className="label-text ">To:</span>
+            <span className="text-gray-400 font-bold">John Doe</span>
+          </div>
+          <Messages />
+          <MessageInput />
+        </>
+      )}
     </div>
   );
 };
