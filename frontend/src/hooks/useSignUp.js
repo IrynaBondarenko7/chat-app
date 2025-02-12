1349f4216f4e16e54a10c88a441363575ac72158
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-12 16:01:30 +0000

    added signup inputs control

---
+++ frontend/src/hooks/useSignUp.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import toast from "react-hot-toast";
+
+const useSignUp = () => {
+  const [loading, setLoading] = useState(false);
+
+  const signup = async ({
+    fullName,
+    username,
+    password,
+    confirmPassword,
+    gender,
+  }) => {
+    const success = handleInputErrors({
+      fullName,
+      username,
+      password,
+      confirmPassword,
+      gender,
+    });
+
+    if (!success) return;
+  };
+};
+
+export default useSignUp;
+
+function handleInputErrors({
+  fullName,
+  username,
+  password,
+  confirmPassword,
+  gender,
+}) {
+  if (!fullName || !username || !password || !confirmPassword || !gender) {
+    toast.error("Please fill all fields");
+    return false;
+  }
+
+  if (password !== confirmPassword) {
+    toast.error("Password do not match");
+    return false;
+  }
+
+  if (password.length < 6) {
+    toast.error("Password must be at least 6 characters");
+    return false;
+  }
+
+  return true;
+}
--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -1,6 +1,25 @@
+import { Link } from "react-router-dom";
 import GenderCheckbox from "./GenderCheckbox";
+import { useState } from "react";
 
 const SignUp = () => {
+  const [inputs, setInputs] = useState({
+    fullName: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    gender: "",
+  });
+
+  const handleCheckboxChange = (gender) => {
+    setInputs({ ...inputs, gender });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(inputs);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-w-96 mx-auto">
       <div className="w-full p-6 rounded-lg shadow-md bg-gray-400/0 bg-clip-padding backdrop-blur-lg">
@@ -8,7 +27,7 @@ const SignUp = () => {
           SignUp
           <span className="text-green-700 ml-2">ChatApp</span>
         </h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label className="label p-2">
               <span className="text-base label-text">Full name</span>
@@ -17,6 +36,10 @@ const SignUp = () => {
               type="text"
               placeholder="John Doe"
               className="w-full input input-bordered h-10"
+              value={inputs.fullName}
+              onChange={(e) => {
+                setInputs({ ...inputs, fullName: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -27,6 +50,10 @@ const SignUp = () => {
               type="text"
               placeholder="johndoe"
               className="w-full input input-bordered h-10"
+              value={inputs.username}
+              onChange={(e) => {
+                setInputs({ ...inputs, username: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -37,6 +64,10 @@ const SignUp = () => {
               type="password"
               placeholder="Enter Password"
               className="w-full input input-bordered h-10"
+              value={inputs.password}
+              onChange={(e) => {
+                setInputs({ ...inputs, password: e.target.value });
+              }}
             />
           </div>
           <div>
@@ -47,16 +78,23 @@ const SignUp = () => {
               type="password"
               placeholder="Confirm Password"
               className="w-full input input-bordered h-10"
+              value={inputs.confirmPassword}
+              onChange={(e) => {
+                setInputs({ ...inputs, confirmPassword: e.target.value });
+              }}
             />
           </div>
-          <GenderCheckbox />
+          <GenderCheckbox
+            onCheckboxChange={handleCheckboxChange}
+            selectedGender={inputs.gender}
+          />
 
-          <a
-            href="#"
+          <Link
+            to="/login"
             className="text-sm hover:underline hover:text-green-950 mt-2 inline-block"
           >
             Already have an account?
-          </a>
+          </Link>
 
           <button className="btn btn-block btn-sm mt-2">Sign Up</button>
         </form>
