4ef86e9846a06bea9db96b53467727405932fea0
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-19 15:37:49 +0000

    added cors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -13,16 +14,13 @@ const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
-
-// app.get("/", (req, res) => {
-//   res.send("test");
-// });
 
 app.listen(PORT, () => {
   connectToMongoDB();
