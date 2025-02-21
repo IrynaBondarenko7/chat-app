fd68ca147598d85f32fe091cacc0f50fe6e27ff6
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-21 12:05:55 +0000

    added conversetions fetch & installed zustand library

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,23 +1,20 @@
-const Conversation = () => {
+const Conversation = ({ conversation, emoji, lastIdx }) => {
   return (
     <>
       <div className="flex gap-2 items-center hover:bg-green-600 rounded px-2 py-1 cursor-pointer">
         <div className="avatar online">
           <div className="w-12 rounded-full">
-            <img
-              src="https://png.pngtree.com/png-clipart/20210915/ourmid/pngtree-user-avatar-placeholder-black-png-image_3918427.jpg"
-              alt="user avatar"
-            />
+            <img src={conversation.profilePic} alt="user avatar" />
           </div>
         </div>
         <div className="flex flex-col flex-1">
           <div className="flex gap-3 justify-between">
-            <p className="font-bold text-gray-200">John Doe</p>
-            <span className="text-xl">😊</span>
+            <p className="font-bold text-gray-200">{conversation.fullName}</p>
+            <span className="text-xl">{emoji}</span>
           </div>
         </div>
       </div>
-      <div className="divider my-0 py-0 h-1" />
+      {!lastIdx && <div className="divider my-0 py-0 h-1" />}
     </>
   );
 };
