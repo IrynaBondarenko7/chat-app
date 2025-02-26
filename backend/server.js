1ff927d6763a236c4246eef808e83e5726ec92d1
Iryna Bondarenko <ira.bondarenko@ukr.net>
2025-02-26 13:57:35 +0000

    added sоcket.io server & socketContext & online users

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import cors from "cors";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
@@ -22,7 +20,7 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   connectToMongoDB();
   console.log(`Server Running on port ${PORT}`);
 });
